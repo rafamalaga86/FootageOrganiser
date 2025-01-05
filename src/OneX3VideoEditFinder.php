f3d283dd45071a6eaa253f34f1a2337c36194d22
Rafael <rafamalaga86@gmail.com>
2025-01-05 18:18:48 -0400

    Improved rendered video finder out of place

--- src/OneX3VideoEditFinder.php
+++ src/OneX3VideoEditFinder.php
@@ -4,7 +4,6 @@ namespace FootageOrganiser;
 
 class OneX3VideoEditFinder
 {
-    // const ONEX3_VIDEO_EDIT = '/\d{4}\d{2}\d{2}_\d{2}\d{2}\d{2}_\d{3}\.mp4$/';
 
     const ONEX3_VIDEO_EDIT = '/^(20\d{2})(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])_([0-1][0-9]|2[0-3])([0-5][0-9])([0-5][0-9])_(\d{3}).mp4$/';
 
